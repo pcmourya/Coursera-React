b50f138e642c23ad02876e3a31a0c773084e2165
pcmourya <prakashiaf1999@gmail.com>
2020-05-25 13:24:10 +0530

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import { Navbar, NavbarBrand } from "reactstrap";
-import "./App.css";
 import Menu from "./components/MenuComponent";
-import {DISHES} from "./shared/dishes";
+import { DISHES } from "./shared/dishes";
+import { Navbar, NavbarBrand } from "reactstrap";
 
-class App extends Component{
-
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
     };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
-render {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
-}
-
 export default App;
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { Card, CardBody, CardImg, CardTitle, CardText } from "reactstrap";
+import dateFormat from "dateformat";
+
+class DishDetailComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dish: this.props.dish,
+    };
+  }
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <Card>
+          <CardImg width="100%" object src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+  renderComment(dish) {
+    if (dish != null) {
+      return (
+        <div>
+          <ul>
+            {dish.comments.map((comments) => {
+              const date = comments.date;
+              return (
+                <div>
+                  {comments.comment}
+                  <br />
+                  <br />
+                  -- {comments.author} ,{dateFormat(date, "mmmm dS, yyyy")}{" "}
+                  <br />
+                  <br />
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+  render() {
+    // const dish=this.props.dish
+    if (this.props.dish != null) {
+      return (
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.props.dish)}
+          </div>
+          <div className="col-4 col-md-5 m-1">
+            <div>
+              <h4>Comments</h4>
+            </div>
+            {this.renderComment(this.props.dish)}
+          </div>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+}
+
+export default DishDetailComponent;
